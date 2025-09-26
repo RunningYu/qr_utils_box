ecbcdd86b50a24b54e6c4b03b351875f4f30a676
RunningYu.github.io <947219346@qq.com>
2025-09-26 23:34:00 +0800

    init

---
+++ src/main/java/com/example/qr_utils_box/QrUtilsBoxApplication.java
@@ -0,0 +1,16 @@
+package com.example.qr_utils_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 其然乐衣Letitbe
+ */
+@SpringBootApplication
+public class QrUtilsBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrUtilsBoxApplication.class, args);
+    }
+
+}
